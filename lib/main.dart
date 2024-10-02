2efa262a2347e2cbf3ef0938f4597d9814d56c29
Anchal1192 <ahalawatanchal@gmail.com>
2024-10-02 16:00:56 +0530

    textfield

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
@@ -21,9 +19,96 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: BlocProvider(
         create: (context) => PostBloc(repository: PostRepository())..add(FetchPosts()),
-        child: HomePage(),
+        child: DynamicTextFields(),
       ),
     );
   }
 }
+class DynamicTextFields extends StatefulWidget {
+  @override
+  _DynamicTextFieldsState createState() => _DynamicTextFieldsState();
+}
+
+class _DynamicTextFieldsState extends State<DynamicTextFields> {
+
+  final List<TextEditingController> _controllers = [TextEditingController()];
+
+  @override
+  void dispose() {
+    for (var controller in _controllers) {
+      controller.dispose();
+    }
+    super.dispose();
+  }
+
+  void _addTextField() {
+    setState(() {
+      _controllers.add(TextEditingController());
+    });
+  }
+
+  void _removeTextField(int index) {
+    setState(() {
+      _controllers[index].dispose();
+      _controllers.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        title: const Text('Adding TextField',style: TextStyle(color: Colors.white),),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: _controllers.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      // Expanded TextField
+                      Expanded(
+                        child: TextField(
+                          controller: _controllers[index],
+                          decoration: InputDecoration(
+                            labelText: 'Enter text ${index + 1}',
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.delete, color: Colors.red),
+                        onPressed: () {
+                          if (_controllers.length > 1) {
+                            _removeTextField(index);
+                          }
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+          InkWell(
+            onTap: _addTextField,
+            child: Container(
+              margin: EdgeInsets.all(20),
+              width: MediaQuery.of(context).size.width,
+              color: Colors.purple,
+              alignment: Alignment.center,
+              height: 40,
+              child: const Text('Add TextField',style: TextStyle(color: Colors.white),),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
 
